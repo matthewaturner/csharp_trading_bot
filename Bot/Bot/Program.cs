4728cf7a8986be58913c5bb2d08dcbafee02ff2e
matthew <matthewaturner@gmail.com>
2021-10-29 11:51:48 -0500

    docker stuff

--- Bot/Bot/Program.cs
+++ Bot/Bot/Program.cs
@@ -1,24 +1,21 @@
 ﻿using Bot.Configuration;
-using Bot.Models;
 using Bot.Strategies;
 using Bot.Engine;
-using Core.Configuration;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Configuration.Json;
 using Microsoft.Extensions.DependencyInjection;
-using RDotNet;
 using System;
 using System.Data.Common;
 using System.Net.Http;
 using Bot.Analyzers;
-using System.Collections.Generic;
 using Bot.Data;
 using System.IO;
 using Bot.Brokers;
-using Core.Azure;
 using Bot.Brokers.BackTest;
 using Newtonsoft.Json;
 using Bot.Analyzers.Loggers;
+using Microsoft.Extensions.Configuration.EnvironmentVariables;
+using System.Collections.Generic;
 
 namespace Bot
 {
@@ -41,10 +38,10 @@ namespace Bot
             IConfiguration configuration = new ConfigurationBuilder()
                 .Add(source: new JsonConfigurationSource()
                 {
-                    Path = "./appsettings.dev.json"
+                    Path = "./appsettings.json"
                 })
+                .Add(source: new EnvironmentVariablesConfigurationSource())
                 .Build();
-            services.Configure<KeyVaultConfiguration>(configuration.GetSection(ConfigurationPaths.KeyVault));
             services.Configure<YahooDataConfiguration>(configuration.GetSection(ConfigurationPaths.Yahoo));
             services.Configure<AlpacaConfiguration>(configuration.GetSection(ConfigurationPaths.Alpaca));
 
@@ -52,7 +49,6 @@ namespace Bot
             DbProviderFactories.RegisterFactory("System.Data.SqlClient", System.Data.SqlClient.SqlClientFactory.Instance);
 
             // inject platform level things
-            services.AddSingleton<IKeyVaultManager, KeyVaultManager>();
             services.AddSingleton<HttpClient>();
 
             // inject data sources
@@ -132,7 +128,15 @@ namespace Bot
             ITradingEngine engine = serviceProvider.GetService<ITradingEngine>();
 
             // read config file
-            string configString = File.ReadAllText(args[0]);
+            string configString = null;
+            if (args.Length > 0 && !string.IsNullOrWhiteSpace(args[0]))
+            {
+                configString = File.ReadAllText(args[0]);
+            }
+            else
+            {
+                configString = GetDefaultConfig();
+            }
             var engineConfig = JsonConvert.DeserializeObject<EngineConfig>(configString);
             
             // write config file to the output directory for future reference
@@ -145,6 +149,50 @@ namespace Bot
 
             engine.Initialize(engineConfig, outputPath);
             engine.RunAsync().Wait();
+        }
+
+        /// <summary>
+        /// Gets default config for test purposes.
+        /// </summary>
+        /// <returns></returns>
+        public static string GetDefaultConfig()
+        {
+            var config = new EngineConfig()
+            {
+                Symbols = new List<string>() { "MSFT" },
+                Interval = Models.TickInterval.Day,
+                Start = new DateTime(2010, 1, 1),
+                End = new DateTime(2020, 1, 1),
+                DataSource = new DependencyConfig()
+                {
+                    Name = "YahooDataSource",
+                    Args = null
+                },
+                Broker = new DependencyConfig()
+                { 
+                    Name = "BackTestingBroker",
+                    Args = new string[] { "10000" }
+                },
+                Strategy = new DependencyConfig()
+                {
+                    Name = "SMACrossoverStrategy",
+                    Args = new string[] { "MSFT", "16", "64", "true" }
+                },
+                Analyzers = new List<DependencyConfig>()
+                { 
+                    new DependencyConfig()
+                    {
+                        Name = "ConsoleLogger",
+                        Args = null
+                    },
+                    new DependencyConfig()
+                    {
+                        Name = "SharpeRatio",
+                        Args = new string[] { "0.00005357" }
+                    }
+                }
+            };
+            return JsonConvert.SerializeObject(config);
         }
     }
 }
