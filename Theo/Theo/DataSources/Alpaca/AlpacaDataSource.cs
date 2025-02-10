4126ad89781cb9c79c223add3c6e7e7dd9112ff3
Matthew Turner <matur@microsoft.com>
2025-02-09 21:25:49 -0600

    latest

--- Theo/Theo/DataSources/Alpaca/AlpacaDataSource.cs
+++ Theo/Theo/DataSources/Alpaca/AlpacaDataSource.cs
@@ -3,10 +3,10 @@ using Microsoft.Extensions.Configuration;
 using System.Collections.Generic;
 using System.Threading.Tasks;
 using System;
-using Theo.Models;
 using System.Net.Http;
 using System.Text.Json;
-using Theo.DataSources.Alpaca.Models;
+
+using Ap = Theo.DataSources.Alpaca.Models;
 
 namespace Theo.DataSources.Alpaca;
 
@@ -19,21 +19,20 @@ public class AlpacaDataSource
         string apiKeyId = config["Alpaca.ApiKeyId"];
         string apiKeySecret = config["Alpaca.ApiKeySecret"];
 
-        _httpClient = new HttpClient();
-        _httpClient.BaseAddress = new Uri("https://data.alpaca.markets/v2/");
+        _httpClient = new HttpClient() { BaseAddress = new Uri("https://data.alpaca.markets/v2/") };
         _httpClient.DefaultRequestHeaders.Add("APCA-API-KEY-ID", apiKeyId);
         _httpClient.DefaultRequestHeaders.Add("APCA-API-SECRET-KEY", apiKeySecret);
     }
 
-    public async Task<List<Models.Bar>> GetHistoricalBarsAsync(string symbol, string timeframe, string start, string end)
+    public async Task<List<Ap.Bar>> GetHistoricalBarsAsync(string symbol, string timeframe, string start, string end)
     {
         var response = await _httpClient.GetAsync($"stocks/{symbol}/bars?timeframe={timeframe}&start={start}&end={end}");
         
         if (response.IsSuccessStatusCode)
         {
             var json = await response.Content.ReadAsStringAsync();
-            var bars = JsonSerializer.Deserialize<BarsResponse>(json);
-            return bars?.Bars ?? new List<Models.Bar>();
+            var bars = JsonSerializer.Deserialize<Ap.BarsResponse>(json);
+            return bars?.Bars ?? [];
         }
 
         throw new Exception($"API call failed: {response.StatusCode}");
