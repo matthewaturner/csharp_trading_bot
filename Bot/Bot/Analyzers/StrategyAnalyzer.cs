e1edbacfcc67562efe380b2e0a9e8cf96570897f
Matthew Turner <matur@microsoft.com>
2025-03-03 15:40:53 -0600

    ignore output files

--- Bot/Bot/Analyzers/StrategyAnalyzer.cs
+++ Bot/Bot/Analyzers/StrategyAnalyzer.cs
@@ -30,32 +30,45 @@ public class StrategyAnalyzer(double annualRiskFreeRate = 0) : IStrategyAnalyzer
     /// </summary>
     public void OnEvent(object sender, MarketDataEvent e)
     {
+        bool isFirstBar = RunResults.PortfolioValues.Count == 0;
         DateTime currentTime = e.Bar.Timestamp;
 
         double currentPortfolioValue = (double)Broker.GetAccount().TotalValue;
         double previousPortfolioValue = RunResults.PortfolioValues.LastOrDefault()?.Value ?? currentPortfolioValue;
         RunResults.PortfolioValues.Add(currentTime, currentPortfolioValue);
 
-        double dailyReturn = (currentPortfolioValue - previousPortfolioValue) / previousPortfolioValue;
-        RunResults.DailyReturns.Add(currentTime, dailyReturn);
+        if (!isFirstBar)
+        {
+            double dailyReturn = (currentPortfolioValue - previousPortfolioValue) / previousPortfolioValue;
+            RunResults.DailyReturns.Add(currentTime, dailyReturn);
 
-        double excessDailyReturn = dailyReturn - (AnnualRiskFreeRate / Engine.Interval.GetIntervalsPerYear());
-        RunResults.ExcessDailyReturns.Add(currentTime, excessDailyReturn);
+            double excessDailyReturn = dailyReturn - (AnnualRiskFreeRate / Engine.Interval.GetIntervalsPerYear());
+            RunResults.ExcessDailyReturns.Add(currentTime, excessDailyReturn);
 
-        double prevCumulativeReturn = RunResults.CumulativeReturns.LastOrDefault()?.Value ?? 0;
-        double cumulativeReturn = (1 + prevCumulativeReturn) * (1 + dailyReturn) - 1;
-        RunResults.CumulativeReturns.Add(currentTime, cumulativeReturn);
+            double prevCumulativeReturn = RunResults.CumulativeReturns.LastOrDefault()?.Value ?? 0;
+            double cumulativeReturn = (1 + prevCumulativeReturn) * (1 + dailyReturn) - 1;
+            RunResults.CumulativeReturns.Add(currentTime, cumulativeReturn);
 
-        double highWaterMark = RunResults.HighWaterMark.LastOrDefault()?.Value ?? 0;
-        if (cumulativeReturn > highWaterMark) highWaterMark = cumulativeReturn;
-        RunResults.HighWaterMark.Add(currentTime, highWaterMark);
+            double highWaterMark = RunResults.HighWaterMark.LastOrDefault()?.Value ?? 0;
+            if (cumulativeReturn > highWaterMark) highWaterMark = cumulativeReturn;
+            RunResults.HighWaterMark.Add(currentTime, highWaterMark);
 
-        double drawdown = (1 + cumulativeReturn) / (1 + highWaterMark) - 1;
-        RunResults.Drawdown.Add(currentTime, drawdown);
+            double drawdown = (1 + cumulativeReturn) / (1 + highWaterMark) - 1;
+            RunResults.Drawdown.Add(currentTime, drawdown);
 
-        double prevDrawdownDuration = RunResults.MaxDrawdownDuration.LastOrDefault()?.Value ?? 0;
-        double maxDrawdownDuration = drawdown < 0 ? prevDrawdownDuration + 1 : 0;
-        RunResults.MaxDrawdownDuration.Add(currentTime, maxDrawdownDuration);
+            double prevDrawdownDuration = RunResults.MaxDrawdownDuration.LastOrDefault()?.Value ?? 0;
+            double maxDrawdownDuration = drawdown < 0 ? prevDrawdownDuration + 1 : 0;
+            RunResults.MaxDrawdownDuration.Add(currentTime, maxDrawdownDuration);
+        }
+        else
+        {
+            RunResults.DailyReturns.Add(currentTime, 0);
+            RunResults.ExcessDailyReturns.Add(currentTime, 0);
+            RunResults.CumulativeReturns.Add(currentTime, 0);
+            RunResults.HighWaterMark.Add(currentTime, currentPortfolioValue);
+            RunResults.Drawdown.Add(currentTime, 0);
+            RunResults.MaxDrawdownDuration.Add(currentTime, 0);
+        }
     }
 
     /// <summary>
