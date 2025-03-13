5050f18b1dca7cffe6ce94b390bc24bb3f6a5a84
Matthew Turner <matur@microsoft.com>
2025-03-12 22:30:14 -0500

    comments and more asserts

--- Bot/IntegrationTests/EpChanExamples/QuantitativeTrading_EpChan_Backtests.cs
+++ Bot/IntegrationTests/EpChanExamples/QuantitativeTrading_EpChan_Backtests.cs
@@ -13,11 +13,17 @@ using static Bot.Engine.TradingEngine;
 
 namespace IntegrationTests.EpChanExamples;
 
-public class EpChanTests
+/// <summary>
+/// Reimplemented backtests for Quantitative Trading by Ernest Chan.
+/// </summary>
+public class QuantitativeTrading_EpChan_Backtests
 {
     // number of decimal places to compare the results to
     const int ACCURACY = 12;
 
+    // Example 3.4:
+    // Buy and hold a share of IGE from 2001-11-26 to 2007-11-14
+    // Calculate the sharpe ratio.
     [Fact]
     public async Task Example3_4()
     {
@@ -36,6 +42,9 @@ public class EpChanTests
         Assert.Equal(0.789317538344851, result.AnnualizedSharpeRatio, ACCURACY);
     }
 
+    // Example 3.5:
+    // Buy one share of IGE and short an equal dollar amount of SPY over the same period.
+    // Calculate the sharpe ratio, maximum drawdown, and maximum drawdown duration.
     [Fact]
     public async Task Example3_5()
     {
@@ -44,14 +53,15 @@ public class EpChanTests
              interval: Interval.OneDay,
              runMode: RunMode.BackTest,
              universe: new() { "IGE", "SPY" },
-             minLogLevel: LogLevel.Debug,
-             shouldWriteCsv: true))
+             minLogLevel: LogLevel.Debug))
          .WithDataSource(new CsvDataSource(GlobalConfig.EpChanDataFolder))
          .WithStrategy(new Ex3_5_LongShort("IGE", "SPY"), 1.0);
 
         var engine = builder.Build();
         RunResult result = await engine.RunAsync();
         Assert.Equal(0.78368110018119, result.AnnualizedSharpeRatio, ACCURACY);
+        Assert.Equal(-0.0952926804720874, result.MaximumDrawdown, ACCURACY);
+        Assert.Equal(497, result.MaximumDrawdownDuration);
     }
 
 }
