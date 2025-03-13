0ab74bb2f5fcc4de79b29d3b71e0f134c05217fb
Matthew Turner <matur@microsoft.com>
2025-03-12 23:08:38 -0500

    simplify allocation definition

--- Bot/Bot/Strategies/EpChan/Ex3_4_BuyAndHold.cs
+++ Bot/Bot/Strategies/EpChan/Ex3_4_BuyAndHold.cs
@@ -5,22 +5,15 @@
 
 using Bot.Models.Allocations;
 using Bot.Models.MarketData;
-using System;
 
 namespace Bot.Strategies.EpChan;
 
-public class Ex3_4_BuyAndHold : StrategyBase
+public class Ex3_4_BuyAndHold(string symbol) : StrategyBase
 {
-    private Allocation alloc;
-
-    public Ex3_4_BuyAndHold(string symbol)
-    {
-        this.alloc = new Allocation();
-        this.alloc.SetAllocation(symbol, 1.0);
-    }
+    private Allocation allocation = new Allocation { [symbol] = 1.0 };
 
     public override Allocation OnMarketData(MarketSnapshot snapshot)
     {
-        return alloc;
+        return allocation;
     }
 }
